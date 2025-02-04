55834410c4c9d4099e9b7070693327956418e365
Oladejo Jephthah <jephthahooh@gmail.com>
2025-02-04 08:52:36 +0100

    tuesday

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,41 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
     }
 
     public function posts(): HasMany
